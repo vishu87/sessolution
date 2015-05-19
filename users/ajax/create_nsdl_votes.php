1ea4ca9002ff44130dba9d30c2f88bd34b5ed530
unknown <vashi@C29P115.bhelhwr.co.in>
2015-05-19 10:00:10 +0530

    NSDL vote file final zip download

--- users/ajax/create_nsdl_votes.php
+++ users/ajax/create_nsdl_votes.php
@@ -1,7 +1,7 @@
 <?php session_start();
 require_once('../../auth.php');
 $separator = '^';
-$line_break = '\n';
+$line_break = '/n';
 $time = strtotime("now");
 $depositories_short = array("1"=>"N","2"=>"C","3"=>"P");
 
@@ -11,7 +11,7 @@ $report_id = mysql_real_escape_string($_GET["id"]);
 $user_info = mysql_query("SELECT * from evoting_info where user_id = $user_id limit 1");
 $row_info = mysql_fetch_array($user_info);
 
-$query = mysql_query("SELECT com_id, even from proxy_ad where id= $report_id limit 1");
+$query = mysql_query("SELECT proxy_ad.com_id, proxy_ad.even, companies.com_name, proxy_ad.meeting_type, proxy_ad.meeting_date from proxy_ad join companies on proxy_ad.com_id = companies.com_id where proxy_ad.id= $report_id limit 1");
 $row_com = mysql_fetch_array($query);
 $com_id = $row_com["com_id"];
 $even = $row_com["even"];
@@ -24,6 +24,11 @@ $qx = "SELECT user_admin_voting.vote_id, user_admin_voting.vote, voting.resoluti
 //echo $qx.'<br>';
 $query_res = mysql_query($qx);
 $total_resolutions = mysql_num_rows($query_res);
+if($total_schemes*$total_resolutions == 0){
+	echo "There are no schemes/resolutions for this meeting";
+	die();
+}
+
 while ($row_res = mysql_fetch_array($query_res)) {
 	array_push($array_res, array("vote"=>$row_res["vote"],"resolution_number"=>$row_res["resolution_number"]));
 }
@@ -39,15 +44,48 @@ for ($i=0; $i < (7-$batch_id_count) ; $i++) {
 
 $batch_id = $str.$batch_id;
 
-$header_line = $batch_id.$separator.'11'.$separator.$row_info["nsdl_mf_id"].$separator.($total_schemes*$total_resolutions).$separator.$even.$separator.date("Ymd",$time).$separator.date("Hi",$time);
-echo $header_line.'<br>';
+$name = name_filter($row_com["com_name"]).'_'.$meeting_types[$row_com["meeting_type"]].'_'.date("d_M_y",$row_com["meeting_date"]).'_'.$batch_id;
+
+$filename = $name.'.txt';
+$file = fopen($filename, "w");
+
+fwrite($file, '{}'.PHP_EOL);
+
+$header_line = $batch_id.$separator.'11'.$separator.$row_info["nsdl_mf_id"].$separator.($total_schemes*$total_resolutions).$separator.$even.$separator.date("Ymd",$time).$separator.date("Hi",$time).PHP_EOL;
+fwrite($file, $header_line);
 
 while ($row_sc = mysql_fetch_array($query_sc)) {
+	$shares_held = $row_sc["shares_held"]*1000;
+	$shares_held_count = strlen($shares_held);
+
+	$str = '';
+	for ($i=0; $i < (18-$shares_held_count) ; $i++) { 
+		$str .= '0';
+	}
+
+	$shares_held = $str.$shares_held;
 	foreach ($array_res as $ar) {
-		$line = $batch_id.$separator.'12'.$separator.$depositories_short[$row_sc["depository"]].$separator.$row_sc["dp_id"].$row_sc["client_id"].$separator.$row_info["nsdl_poa_id"].$separator.$ar["resolution_number"].$separator.$ar["vote"].$separator.'asdsad';
-		echo $line.'<br>';
+		$line = $batch_id.$separator.'12'.$separator.$depositories_short[$row_sc["depository"]].$separator.$row_sc["dp_id"].$row_sc["client_id"].$separator.$row_info["nsdl_poa_id"].$separator.$ar["resolution_number"].$separator.$ar["vote"].$separator.$shares_held.PHP_EOL;
+		fwrite($file, $line);
 	}
 }
+fwrite($file, '{}');
+fclose($file);
 
+$zip = new ZipArchive();
+$zipname = $name.'.zip';
+if($zip->open($zipname, ZIPARCHIVE::CREATE)!==TRUE){ 
+	$error .= "* Sorry ZIP creation failed at this time";
+}
+
+$zip->addFile($filename);
+$zip->close();
+if(file_exists($zipname)){
+	header('Content-type: application/zip');
+	header('Content-Disposition: attachment; filename="'.$zipname.'"');
+	readfile($zipname);
+	unlink($zipname);
+	unlink($filename);
+}
 
 ?>
